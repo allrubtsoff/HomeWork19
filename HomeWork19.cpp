f06e4e1f86284d6ec2f46718daf0eac1cd21f66c
allrubtsoff <allrubtsooff@yandex.ru>
2021-07-17 14:11:02 +0300

    1

--- HomeWork19.cpp
+++ HomeWork19.cpp
@@ -1,49 +1,62 @@
 ﻿#include <iostream>
+#include <cstdlib>
+
 class Animal
 {
 public:
-    virtual void makeSound() const = 0;
+    virtual void Voice() const = 0;
+    virtual ~Animal() 
+    {
+        std::cout << "ClearA\n";
+    }
 };
 
 class Cat : public Animal
 {
 public:
-    void makeSound() const override
+    void Voice() const override
     {
         std::cout << "Meow\n";
     }
+    ~Cat()
+    {
+        std::cout << "ClearC\n";
+    }    
 };
+
 
 class Dog : public Animal
 {
 public:
-    void makeSound() const override
+    void Voice() const override
     {
         std::cout << "Woof\n";
+    }
+    ~Dog() 
+    {
+        std::cout << "ClearD\n";
     }
 };
 
 class Pig : public Animal
 {
 public:
-    void makeSound() const override
+    void Voice() const override
     {
         std::cout << "Oink\n";
+    }
+    ~Pig() 
+    {
+        std::cout << "ClearP\n";
     }
 };
 
 int main()
 {
-    Animal* animals[3];
-    animals[0] = new Cat();
-    animals[1] = new Dog();
-    animals[2] = new Pig();
-
+    Animal *animals[3] = { new Cat, new Dog, new Pig };
+    
     for (Animal* a : animals)
-        a->makeSound();
+        a->Voice();
 
-    delete animals[0];
-    delete animals[1];
-    delete animals[2];
-
+    delete[] animals;
 }
